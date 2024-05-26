0f703ea8878805bd536be9cc95721728c42b093b
Joshua Anderson <andersmmg@gmail.com>
2024-05-26 14:01:12 -0600

    Added block drop loot tables

--- src/main/java/com/andersmmg/lockandblock/datagen/ModLootTableProvider.java
+++ src/main/java/com/andersmmg/lockandblock/datagen/ModLootTableProvider.java
@@ -1,5 +1,6 @@
 package com.andersmmg.lockandblock.datagen;
 
+import com.andersmmg.lockandblock.block.ModBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
 
@@ -10,7 +11,16 @@ public class ModLootTableProvider extends FabricBlockLootTableProvider {
 
     @Override
     public void generate() {
-//        addDrop(ModBlocks.DANGER_IRON_BLOCK);
+        addDrop(ModBlocks.TRIP_MINE);
+        addDrop(ModBlocks.PROX_MINE);
+        addDrop(ModBlocks.LAND_MINE);
+        addDrop(ModBlocks.PLAYER_SENSOR);
+        addDrop(ModBlocks.TESLA_COIL);
+        addDrop(ModBlocks.KEYPAD);
+        addDrop(ModBlocks.KEYCARD_READER);
+        addDrop(ModBlocks.KEYCARD_WRITER);
+        addDrop(ModBlocks.KEYCARD_CLONER);
+        addDrop(ModBlocks.FORCEFIELD_GENERATOR);
     }
 
 }
